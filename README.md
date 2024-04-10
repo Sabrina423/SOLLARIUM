e6187e15a9705e4129158a6cd434afc4ff0e0bbe
Sabrina423 <sassadesabrina@gmail.com>
2024-04-10 12:24:48 -0300

    inicial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# SOLLARIUM
-Site voltado à Energia Solar
+Para adicionar as dependências abra o terminal no VS Code
+e digite:
+
+**** npm config set strict-ssl false
+*para liberar o proxy(se necessário)
+
+**** npm install
---
+++ app/routes/router.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router ();
+
+router.get("/", function (req,res){
+    res.render ("pages/index");
+});
+router.get("html", function (req,res){
+    res.render("pages/html");
+});
+router.get("/jquery", function (req,res){
+    res.render("pages/jquery");
+});
+router.post("html", function (req, res){
+    res.json(req.body);
+});
+router.post("/jquery", function (req, res){
+    console.log(req.body);
+});
+
+module.exports = router;
