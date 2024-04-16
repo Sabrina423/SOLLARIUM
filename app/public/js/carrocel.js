063d095aef844c7e02f89aba4bd66888683af921
Sabrina423 <sassadesabrina@gmail.com>
2024-04-16 09:48:37 -0300

    alteração nos codigos

--- app/public/js/carrocel.js
+++ app/public/js/carrocel.js
@@ -1,11 +1,13 @@
 const slides = document.querySelectorAll('.carousel input[type="radio"]');
 slides.forEach(slide => {
     slide.addEventListener('change', function() {
-        const checkedSlide = document.querySelector('.carousel input[type="radio"]:checked + figcaption');
+        const checkedSlide = document.querySelector('.carousel input[type="radio"]:checked + figure');
+        const allSlides = document.querySelectorAll('.carousel figure');
+        allSlides.forEach(slide => {
+            slide.style.opacity = 0;
+        });
         if (checkedSlide) {
-            checkedSlide.style.opacity = 0;
+            checkedSlide.style.opacity = 1;
         }
-        const selectedCaption = document.querySelector(`.carousel figcaption[for="${this.id}"]`);
-        selectedCaption.style.opacity = 1;
     });
 });
