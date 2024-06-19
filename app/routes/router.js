94712d8a62e02860850a675ea6eb04423d822c75
Sabrina423 <sassadesabrina@gmail.com>
2024-06-19 12:28:46 -0300

    alterações nos códigos

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-// Middleware de autenticação
+
 function isAuthenticated(req, res, next) {
   if (req.session.user) {
     return next();
@@ -9,15 +9,15 @@ function isAuthenticated(req, res, next) {
   res.redirect('/login');
 }
 
-// Página de login
+
 router.get('/login', (req, res) => {
-  res.render('pages/login'); // Certifique-se de ter a página de login
+  res.render('pages/login'); 
 });
 
-// Processar login
+
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
-  // Substitua por sua lógica de autenticação
+
   if (username === 'admin' && password === 'admin') {
     req.session.user = { username };
     res.redirect('/');
@@ -38,24 +38,33 @@ router.get('/logout', (req, res) => {
 });
 
 
-router.get('/', isAuthenticated, (req, res) => {
+router.get('/',  (req, res) => {
   res.render('pages/home');
 });
 
-router.get('/soucliente', isAuthenticated, (req, res) => {
+router.get('/soucliente',  (req, res) => {
   res.render('pages/soucliente');
 });
 
-router.get('/souprofissional', isAuthenticated, (req, res) => {
+router.get('/souprofissional',(req, res) => {
   res.render('pages/souprofissional');
 });
 
-router.get('/cadastroprof', isAuthenticated, (req, res) => {
+router.get('/cadastroprof', (req, res) => {
   res.render('pages/cadastroprof');
 });
 
-router.get('/cadastrocliente', isAuthenticated, (req, res) => {
+router.get('/cadastrocliente',  (req, res) => {
   res.render('pages/cadastrocliente');
 });
+
+router.get('/cadastrocliente' (req, res) => {
+  res.render('pages/orcamento');
+});
+
+router.post('/perfilcliente',  (req, res) => {
+  res.render('pages/perfilciente');
+});
+
 
 module.exports = router;
