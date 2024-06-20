dc7d2dc2c2ee5eec7b6178e1fc1397344123a490
Sabrina423 <sassadesabrina@gmail.com>
2024-06-20 11:11:23 -0300

    atualizações nos codigos

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,44 +1,4 @@
-var express = require('express');
-var router = express.Router();
-
-
-function isAuthenticated(req, res, next) {
-  if (req.session.user) {
-    return next();
-  }
-  res.redirect('/login');
-}
-
-
-router.get('/login', (req, res) => {
-  res.render('pages/login'); 
-});
-
-
-router.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  if (username === 'admin' && password === 'admin') {
-    req.session.user = { username };
-    res.redirect('/');
-  } else {
-    res.redirect('/login');
-  }
-});
-
-
-router.get('/logout', (req, res) => {
-  req.session.destroy(err => {
-    if (err) {
-      return res.redirect('/');
-    }
-    res.clearCookie('connect.sid');
-    res.redirect('/login');
-  });
-});
-
-
-router.get('/',  (req, res) => {
+router.get ('/',  (req, res) => {
   res.render('pages/home');
 });
 
@@ -58,7 +18,7 @@ router.get('/cadastrocliente',  (req, res) => {
   res.render('pages/cadastrocliente');
 });
 
-router.get('/cadastrocliente' (req, res) => {
+router.get('/cadastrocliente', (req, res) => {
   res.render('pages/orcamento');
 });
 
