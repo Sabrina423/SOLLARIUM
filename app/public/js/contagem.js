35795c05fc74e69ef301fe666aa8af77fd1fbc13
Sabrina <122060714+Sabrina423@users.noreply.github.com>
2024-06-26 21:35:29 +0000

    atualização código fonte

---
+++ app/public/js/contagem.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let counter = document.getElementById('counter');
+    let count = 0;
+    setInterval(() => {
+        if (count < 1000) { // Ajuste o valor final conforme necessário
+            count++;
+            counter.innerText = count;
+        }
+    }, 50); // Ajuste a velocidade conforme necessário
+});
