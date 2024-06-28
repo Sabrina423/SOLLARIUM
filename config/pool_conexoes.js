1eebfd900cb8448313fcd636adf045275b49449e
Sabrina423 <sassadesabrina@gmail.com>
2024-06-28 10:59:43 -0300

    pool conexoes

--- config/pool_conexoes.js
+++ config/pool_conexoes.js
@@ -3,7 +3,6 @@ const mysql = require('mysql2')
 const pool = mysql.createPool({
     host: process.env.DB_HOST, 
     user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORDITB,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     waitForConnections: true,
