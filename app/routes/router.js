fdee7f6af347956802b8a474e44595b6c7c2e7bf
Sabrina <122060714+Sabrina423@users.noreply.github.com>
2024-08-13 13:28:22 +0000

    atualização

--- app/routes/router.js
+++ app/routes/router.js
@@ -65,30 +65,31 @@ router.get('/feedback', authenticateToken, (req, res) => {
 });
 
 //Rota de registro cliente
-router.post('/cadastrocliente', [
-    body('username')
+router.post('/cadastrocliente', 
+    body('nome_cliente')
         .isString().withMessage('O nome de usuário deve ser uma string')
         .isLength({ min: 3, max: 45 }).withMessage('O nome de usuário deve ter entre 3 e 45 caracteres'),
-    body('password')
+    body('senha_cliente')
         .isLength({ min: 8 }).withMessage('A senha deve ter pelo menos 8 caracteres')
         .matches(/[A-Z]/).withMessage('A senha deve conter pelo menos uma letra maiúscula')
         .matches(/[a-z]/).withMessage('A senha deve conter pelo menos uma letra minúscula')
         .matches(/\d/).withMessage('A senha deve conter pelo menos um número')
         .matches(/[@$!%*?&]/).withMessage('A senha deve conter pelo menos um caractere especial'),
-    body('cpf')
+    body('cpf_cliente')
         .isLength({ min: 11, max: 11 }).withMessage('O CPF deve ter 11 dígitos')
         .isNumeric().withMessage('O CPF deve conter apenas números'),
-    body('endereco')
-        .isString().withMessage('O endereço deve ser uma string')
-        .isLength({ min: 10, max: 100 }).withMessage('O endereço deve ter entre 10 e 100 caracteres'),
-    body('contato')
+    body('cep_cliente')
+        .isString().withMessage('O cep deve ser uma string')
+        .isLength({ min: 8, max: 8 }).withMessage('O cep deve ter entre 10 e 100 caracteres'),
+    body('contato_cliente')
         .isLength({ min: 10, max: 15 }).withMessage('O contato deve ter entre 10 e 15 dígitos')
         .isNumeric().withMessage('O contato deve conter apenas números'),
-    body('email')
+    body('email_cliente')
         .isEmail().withMessage('O e-mail deve ser válido')
         .normalizeEmail()
-], async (req, res) => {
+, async (req, res) => {
     const errors = validationResult(req);
+    console.log(errors)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
@@ -105,11 +106,12 @@ router.post('/cadastrocliente', [
     };
 
     try {
-        await cliente.create(dadosForm);
-        res.status(200).json({ message: 'Cadastro realizado com sucesso!' });
+        let result = await cliente.create(dadosForm);
+        console.log(result);
+       // res.status(200).json({ message: 'Cadastro realizado com sucesso!' });
     } catch (e) {
         console.error(e);
-        res.status(500).json({ error: 'Erro ao registrar o cliente.' });
+       // res.status(500).json({ error: 'Erro ao registrar o cliente.' });
     }
 });
 
