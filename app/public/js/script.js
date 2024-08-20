dde161a9f0e9eeeb03f8ce1bbf77d449d455975a
Sabrina <122060714+Sabrina423@users.noreply.github.com>
2024-08-20 15:07:12 +0000

    commit login

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -15,3 +15,23 @@ function notify(titulo, texto, tipo, posicao,duracao=3000) {
         position:posicao 
     })
 }
+
+function notify(titulo, texto, tipo, posicao,duracao=3000) {
+    new Notify({
+        status: tipo,
+        title: titulo,
+        text:texto,
+        effect: 'fade',
+        speed: 500,
+        showIcon: true,
+        showCloseButton: true,
+        autoclose: true,
+        autotimeout: duracao,
+        gap: 20,
+        distance: 20,
+        type: 1,
+        position:posicao 
+    })
+}
+
+
