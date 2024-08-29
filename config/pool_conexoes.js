65ff578c36b1abc6333039707c17a0ec44265b9e
MariaEDaitx <133165477+MariEDaitx@users.noreply.github.com>
2024-08-29 13:57:49 +0000

    profissional+banco

--- config/pool_conexoes.js
+++ config/pool_conexoes.js
@@ -4,6 +4,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST, 
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD, 
+  // password: process.env.DB_PASSWORDITB, 
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   waitForConnections: true,
